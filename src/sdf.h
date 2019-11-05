76a8821690f5a9d1c9e14c10b9e17c6dd8d95313
Lorcan Mc Donagh <lorcan@lmdsp.com>
2019-11-05 22:05:54 +0100

    Modern C++ update #2

--- src/sdf.h
+++ src/sdf.h
@@ -95,14 +95,21 @@ constexpr precision_t black = 0.001f;
 /// sqrt(2)
 constexpr precision_t sqrt_2 = 1.4142136f;
 
+/// 1 / sqrt(2)
+constexpr precision_t sqrt_2_inv = 1.0f / sqrt_2;
+
 /// Big value used to initialize the distance field.
 constexpr precision_t big_value = 1e+37f;
 
 }
 
+namespace detail {
+
 [[nodiscard]] constexpr float sdf__clamp01(float x) noexcept
 {
 	return (x < 0.0f) ? 0.0f : ((x > 1.0f) ? 1.0f : x);
+}
+
 }
 
 void sdfCoverageToDistanceField(pixel_t* out, coord_t outstride, const pixel_t* img, coord_t width, coord_t height, coord_t stride)
@@ -110,25 +117,25 @@ void sdfCoverageToDistanceField(pixel_t* out, coord_t outstride, const pixel_t* 
 	using constants::sqrt_2;
 
 	// Zero out borders
-	for (coord_t x = 0; x < width; x++)
+	for (coord_t x = 0; x < width; ++x)
 	{
 		out[x] = 0;
 	}
 
-	for (coord_t y = 1; y < height; y++)
+	for (coord_t y = 1; y < height; ++y)
 	{
 		out[y * outstride] = 0;
 		out[width - 1 + y * outstride] = 0;
 	}
 
-	for (coord_t x = 0; x < width; x++)
+	for (coord_t x = 0; x < width; ++x)
 	{
 		out[x + (height - 1) * outstride] = 0;
 	}
 
-	for (coord_t y = 1; y < height-1; y++) 
+	for (coord_t y = 1; y < height - 1; ++y)
 	{
-		for (coord_t x = 1; x < width-1; x++) 
+		for (coord_t x = 1; x < width-1; ++x) 
 		{
 			const coord_t k = x + y * stride;
 
@@ -155,27 +162,32 @@ void sdfCoverageToDistanceField(pixel_t* out, coord_t outstride, const pixel_t* 
 				}
 			}
 
-			precision_t gx = -(float)img[k - stride - 1] - sqrt_2 * (float)img[k - 1] - (float)img[k + stride - 1] + (float)img[k - stride + 1] + sqrt_2 * (float)img[k + 1] + (float)img[k + stride + 1];
-			precision_t gy = -(float)img[k - stride - 1] - sqrt_2 * (float)img[k - stride] - (float)img[k - stride + 1] + (float)img[k + stride - 1] + sqrt_2 * (float)img[k + stride] + (float)img[k + stride + 1];
+			const precision_t px_ms_m1 = static_cast<precision_t>(img[k - stride - 1]);
+			const precision_t px_ms_p1 = static_cast<precision_t>(img[k - stride + 1]);
+			const precision_t px_ps_m1 = static_cast<precision_t>(img[k + stride - 1]);
+			const precision_t px_ps_p1 = static_cast<precision_t>(img[k + stride + 1]);
 
-			const precision_t a = (float)img[k] / 255.0f;
+			precision_t gx = -px_ms_m1 - sqrt_2 * static_cast<precision_t>(img[k - 1]) - px_ps_m1 + px_ms_p1 + sqrt_2 * static_cast<precision_t>(img[k + 1]) + px_ps_p1;
+			precision_t gy = -px_ms_m1 - sqrt_2 * static_cast<precision_t>(img[k - stride]) - px_ms_p1 + px_ps_m1 + sqrt_2 * static_cast<precision_t>(img[k + stride]) + px_ps_p1;
+
+			const precision_t a = static_cast<precision_t>(img[k]) / 255.0f;
 
 			gx = std::fabs(gx);
 			gy = std::fabs(gy);
 
 			precision_t d = 0.0f;
 
-			if (gx < 0.0001f || gy < 0.000f)
+			if ((gx < 0.0001f) || (gy < 0.000f))
 			{
 				d = (0.5f - a) * sqrt_2;
 			}
 			else
 			{
-				precision_t glen = gx * gx + gy * gy;
+				const precision_t glen2 = gx * gx + gy * gy;
+				const precision_t gnorm = 1.0f / std::sqrt(glen2);
 
-				glen = 1.0f / std::sqrt(glen);
-				gx *= glen;
-				gy *= glen;
+				gx *= gnorm;
+				gy *= gnorm;
 
 				if (gx < gy) 
 				{
@@ -191,245 +203,368 @@ void sdfCoverageToDistanceField(pixel_t* out, coord_t outstride, const pixel_t* 
 				}
 				else if (a < (1.0 - a1))
 				{ 
-					// a1 <= a <= 1-a1
+					// a1 <= a <= 1 - a1
 					d = (0.5f - a) * gx;
 				}
 				else
 				{ 
-					// 1-a1 < a <= 1
+					// 1 - a1 < a <= 1
 					d = -0.5f * (gx + gy) + std::sqrt(2.0f * gx * gy * (1.0f - a));
 				}
 			}
 
-			d *= 1.0f / sqrt_2;
+			d *= constants::sqrt_2_inv;
 
-			out[x + y * outstride] = static_cast<pixel_t>(sdf__clamp01(0.5f - d) * 255.0f);
+			out[x + y * outstride] = static_cast<pixel_t>(detail::sdf__clamp01(0.5f - d) * 255.0f);
 		}
 	}
 }
 
-static float sdf__edgedf(float gx, float gy, float a)
+namespace detail {
+
+[[nodiscard]] precision_t sdf_edgedf(precision_t gx, precision_t gy, precision_t a) noexcept
 {
-	float df, a1;
-	if ((gx == 0) || (gy == 0)) {
+	if ((gx == 0) || (gy == 0)) 
+	{
 		// Either A) gu or gv are zero, or B) both
 		// Linear approximation is A) correct or B) a fair guess
-		df = 0.5f - a;
-	} else {
-		// Everything is symmetric wrt sign and transposition,
-		// so move to first octant (gx>=0, gy>=0, gx>=gy) to
-		// avoid handling all possible edge directions.
-		gx = fabsf(gx);
-		gy = fabsf(gy);
-		if (gx < gy) {
-			float temp = gx;
-			gx = gy;
-			gy = temp;
-		}
-		a1 = 0.5f*gy/gx;
-		if (a < a1) { // 0 <= a < a1
-			df = 0.5f*(gx + gy) - sqrtf(2.0f*gx*gy*a);
-		} else if (a < (1.0-a1)) { // a1 <= a <= 1-a1
-			df = (0.5f-a)*gx;
-		} else { // 1-a1 < a <= 1
-			df = -0.5f*(gx + gy) + sqrt(2.0f*gx*gy*(1.0f-a));
-		}
+		return 0.5f - a;
 	}
-	return df;
+	
+	// Everything is symmetric w.r.t sign and transposition,
+	// so move to first octant (gx >= 0, gy >= 0, gx >= gy) to
+	// avoid handling all possible edge directions.
+	gx = std::fabs(gx);
+	gy = std::fabs(gy);
+
+	if (gx < gy) 
+	{
+		std::swap(gx, gy);
+	}
+
+	const precision_t a1 = 0.5f * gy / gx;
+
+	if (a < a1) 
+	{ 
+		// 0 <= a < a1
+		return 0.5f * (gx + gy) - std::sqrt(2.0f * gx * gy * a);
+	}
+	 
+	if (a < (1.0 - a1))
+	{ 
+		// a1 <= a <= 1 - a1
+		return (0.5f - a) * gx;
+	}
+
+	// 1 - a1 < a <= 1
+	return -0.5f * (gx + gy) + std::sqrt(2.0f * gx * gy * (1.0f - a));
 }
 
-struct SDFpoint {
-	float x,y;
+}
+
+struct Point 
+{
+	precision_t x;
+	precision_t y;
+
+	[[nodiscard]] precision_t distance_squared(Point other) const noexcept
+	{
+		const precision_t dx = other.x - x;
+		const precision_t dy = other.y - y;
+
+		return dx * dx + dy * dy;
+	}
 };
 
-static float sdf__distsqr(struct SDFpoint* a, struct SDFpoint* b)
-{
-	float dx = b->x - a->x, dy = b->y - a->y;
-	return dx*dx + dy*dy;
-}
-
-void sdfBuildDistanceFieldNoAlloc(unsigned char* out, int outstride, float radius,
-								  const unsigned char* img, int width, int height, int stride,
-								  unsigned char* temp)
+void sdfBuildDistanceFieldNoAlloc(pixel_t* out, coord_t outstride, float radius, const pixel_t* img, coord_t width, coord_t height, coord_t stride, pixel_t* temp)
 {
 	using constants::sqrt_2;
 
-	int i, x, y, pass;
-	float scale;
-	float* tdist = (float*)&temp[0];
-	struct SDFpoint* tpt = (struct SDFpoint*)&temp[width * height * sizeof(float)];
+	const coord_t pixel_count = width * height;
 
+	precision_t* const __restrict tdist = reinterpret_cast<precision_t*>(&temp[0]);
+	Point* const __restrict tpt = reinterpret_cast<Point*>(&temp[pixel_count * sizeof(precision_t)]);
+	
 	// Initialize buffers
-	for (i = 0; i < width*height; i++) {
+	for (coord_t i = 0; i < pixel_count; ++i)
+	{
 		tpt[i].x = 0;
 		tpt[i].y = 0;
 		tdist[i] = constants::big_value;
 	}
 
 	// Calculate position of the anti-aliased pixels and distance to the boundary of the shape.
-	for (y = 1; y < height-1; y++) {
-		for (x = 1; x < width-1; x++) {
-			int tk, k = x + y * stride;
-			struct SDFpoint c = { (float)x, (float)y };
-			float d, gx, gy, glen;
+	const coord_t height_m1 = height - 1;
+	const coord_t width_m1 = width - 1;
+
+	for (coord_t y = 1; y < height_m1; ++y)
+	{
+		for (coord_t x = 1; x < width_m1; x++) 
+		{
+			 const coord_t k = x + y * stride;
 
 			// Skip flat areas.
-			if (img[k] == 255) continue;
-			if (img[k] == 0) {
+			if (255 == img[k])
+			{
+				continue;
+			}
+
+			if (0 == img[k])
+			{
 				// Special handling for cases where full opaque pixels are next to full transparent pixels.
 				// See: https://github.com/memononen/SDF/issues/2
-				int he = img[k-1] == 255 || img[k+1] == 255;
-				int ve = img[k-stride] == 255 || img[k+stride] == 255;
-				if (!he && !ve) continue;
+				const bool he = (img[k - 1] == 255) || (img[k + 1] == 255);
+				const bool ve = (img[k - stride] == 255) || (img[k + stride] == 255);
+
+				if (!he && !ve)
+				{
+					continue;
+				}
 			}
 
 			// Calculate gradient direction
-			gx = -(float)img[k-stride-1] - sqrt_2 *(float)img[k-1] - (float)img[k+stride-1] + (float)img[k-stride+1] + sqrt_2 *(float)img[k+1] + (float)img[k+stride+1];
-			gy = -(float)img[k-stride-1] - sqrt_2 *(float)img[k-stride] - (float)img[k-stride+1] + (float)img[k+stride-1] + sqrt_2 *(float)img[k+stride] + (float)img[k+stride+1];
-			if (fabsf(gx) < 0.001f && fabsf(gy) < 0.001f) continue;
-			glen = gx*gx + gy*gy;
-			if (glen > 0.0001f) {
-				glen = 1.0f / sqrtf(glen);
+			precision_t gx = -(float)img[k - stride - 1] - sqrt_2 * (float)img[k - 1] - (float)img[k + stride - 1] + (float)img[k - stride + 1] + sqrt_2 * (float)img[k + 1] + (float)img[k + stride + 1];
+			precision_t gy = -(float)img[k - stride - 1] - sqrt_2 * (float)img[k - stride] - (float)img[k - stride + 1] + (float)img[k + stride - 1] + sqrt_2 * (float)img[k + stride] + (float)img[k + stride + 1];
+
+			if ((std::fabs(gx) < 0.001f) && (std::fabs(gy) < 0.001f))
+			{
+				continue;
+			}
+
+			precision_t glen = gx * gx + gy * gy;
+			
+			if (glen > 0.0001f) 
+			{
+				glen = 1.0f / std::sqrt(glen);
 				gx *= glen;
 				gy *= glen;
 			}
 
 			// Find nearest point on contour.
-			tk = x + y * width;
-			d = sdf__edgedf(gx, gy, (float)img[k]/255.0f);
-			tpt[tk].x = x + gx*d;
-			tpt[tk].y = y + gy*d;
-			tdist[tk] = sdf__distsqr(&c, &tpt[tk]);
+			const coord_t tk = x + y * width;
+			const precision_t d = detail::sdf_edgedf(gx, gy, static_cast<precision_t>(img[k]) / 255.0f);
+
+			tpt[tk].x = x + gx * d;
+			tpt[tk].y = y + gy * d;
+
+			const Point c = { static_cast<precision_t>(x), static_cast<precision_t>(y) };
+
+			tdist[tk] = c.distance_squared(tpt[tk]);
 		}
 	}
 
 	using constants::black;
 
 	// Calculate distance transform using sweep-and-update.
-	for (pass = 0; pass < constants::max_passes; pass++){
+	for (int pass = 0; pass < constants::max_passes; ++pass)
+	{
 		int changed = 0;
 
 		// Bottom-left to top-right.
-		for (y = 1; y < height-1; y++) {
-			for (x = 1; x < width-1; x++) {
-				int k = x+y*width, kn, ch = 0;
-				struct SDFpoint c = { (float)x, (float)y }, pt;
-				float pd = tdist[k], d;
-				// (-1,-1)
-				kn = k - 1 - width;
-				if (tdist[kn] < pd) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+		for (coord_t y = 1; y < height_m1; ++y) 
+		{
+			for (coord_t x = 1; x < width_m1; ++x) 
+			{
+				const coord_t k = x + y * width;
+				const Point c = { static_cast<precision_t>(x), static_cast<precision_t>(y) };
+
+				Point pt{};
+				precision_t pd = tdist[k];
+				bool ch = false;
+			
+				// (-1, -1)
+				{
+					const coord_t kn = k - 1 - width;
+
+					if (tdist[kn] < pd)
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
-				// (0,-1)
-				kn = k - width;
-				if (tdist[kn] < pd) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+
+				// (0, -1)
+				{
+					const coord_t kn = k - width;
+
+					if (tdist[kn] < pd)
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
+
 				// (1,-1)
-				kn = k + 1 - width;
-				if (tdist[kn] < pd) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+				{
+					const coord_t kn = k + 1 - width;
+
+					if (tdist[kn] < pd)
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
-				// (-1,0)
-				kn = k - 1;
-				if (tdist[kn] < tdist[k]) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+
+				// (-1, 0)
+				{
+					const coord_t kn = k - 1;
+
+					if (tdist[kn] < tdist[k])
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
-				if (ch) {
+
+				if (ch) 
+				{
 					tpt[k] = pt;
 					tdist[k] = pd;
-					changed++;
+
+					++changed;
 				}
 			}
 		}
 
 		// Top-right to bottom-left.
-		for (y = height-2; y > 0 ; y--) {
-			for (x = width-2; x > 0; x--) {
-				int k = x+y*width, kn, ch = 0;
-				struct SDFpoint c = { (float)x, (float)y }, pt;
-				float pd = tdist[k], d;
-				// (1,0)
-				kn = k + 1;
-				if (tdist[kn] < pd) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+		const coord_t height_m2 = height - 2;
+		const coord_t width_m2 = width - 2;
+
+		for (coord_t y = height_m2; y > 0 ; --y)
+		{
+			for (coord_t x = width_m2; x > 0; --x) 
+			{
+				const coord_t k = x + y * width;
+				const Point c = { static_cast<precision_t>(x), static_cast<precision_t>(y) };
+
+				Point pt{};
+				bool ch = false;
+				precision_t pd = tdist[k];
+
+				// (1, 0)
+				{
+					const coord_t kn = k + 1;
+
+					if (tdist[kn] < pd)
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
+
 				// (-1,1)
-				kn = k - 1 + width;
-				if (tdist[kn] < pd) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+				{
+					const coord_t kn = k - 1 + width;
+
+					if (tdist[kn] < pd)
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
-				// (0,1)
-				kn = k + width;
-				if (tdist[kn] < pd) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+
+				// (0, 1)
+				{
+					const coord_t kn = k + width;
+
+					if (tdist[kn] < pd)
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
-				// (1,1)
-				kn = k + 1 + width;
-				if (tdist[kn] < pd) {
-					d = sdf__distsqr(&c, &tpt[kn]);
-					if (d + black < pd) {
-						pt = tpt[kn];
-						pd = d;
-						ch = 1;
+
+				// (1, 1)
+				{
+					const coord_t kn = k + 1 + width;
+
+					if (tdist[kn] < pd)
+					{
+						const precision_t d = c.distance_squared(tpt[kn]);
+
+						if ((d + black) < pd)
+						{
+							pt = tpt[kn];
+							pd = d;
+							ch = true;
+						}
 					}
 				}
-				if (ch) {
+
+				if (ch) 
+				{
 					tpt[k] = pt;
 					tdist[k] = pd;
-					changed++;
+					++changed;
 				}
 			}
 		}
 
-		if (changed == 0) break;
-	}
-
-	// Map to good range.
-	scale = 1.0f / radius;
-	for (y = 0; y < height; y++) {
-		for (x = 0; x < width; x++) {
-			float d = sqrtf(tdist[x+y*width]) * scale;
-			if (img[x+y*stride] > 127) d = -d;
-			out[x+y*outstride] = (unsigned char)(sdf__clamp01(0.5f - d*0.5f) * 255.0f);
+		if (0 == changed)
+		{
+			break;
 		}
 	}
 
+	// Map to good range.
+	const precision_t scale = 1.0f / radius;
+
+	for (coord_t y = 0; y < height; ++y)
+	{
+		for (coord_t x = 0; x < width; ++x)
+		{
+			precision_t d = std::sqrt(tdist[x + y * width]) * scale;
+
+			if (img[x + y * stride] > 127)
+			{
+				d = -d;
+			}
+
+			out[x + y * outstride] = static_cast<pixel_t>(detail::sdf__clamp01(0.5f - d * 0.5f) * 255.0f);
+		}
+	}
 }
 
 int sdfBuildDistanceField(unsigned char* out, int outstride, float radius,
